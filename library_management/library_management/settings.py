0ee5999a3ce0b5d13dc2d2030df4423860423d67
utkarshpandey-betaflux <utkarsh.pandey@betaflux.co>
2023-05-07 08:18:30 +0530

    removed U+2002 characters

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql',
-'URL': os.getenv('POSTGRES_URL'),
-'NAME': os.getenv('PGNAME'),
-'USER': os.getenv('PGUSER'),
-'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-'HOST': os.getenv('PGHOST'),
-'PORT': os.getenv('PGPORT'),
+'default':{
+'ENGINE':'django.db.backends.postgresql',
+'URL':os.getenv('POSTGRES_URL'),
+'NAME':os.getenv('PGNAME'),
+'USER':os.getenv('PGUSER'),
+'PASSWORD':os.getenv('POSTGRES_PASSWORD'),
+'HOST':os.getenv('PGHOST'),
+'PORT':os.getenv('PGPORT'),
 }
 }
 
